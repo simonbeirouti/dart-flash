0716726708ce0cb717bf3220d588214483ce3be4
Simon Beirouti <hello@simonbeirouti.com>
2022-03-30 16:13:36 +1100

    simplified calculation issue

--- src/logic.dart
+++ src/logic.dart
@@ -47,31 +47,7 @@ void consolePrinter(int number) {
 }
 
 void rewardCeremony(int fS) {
-  double average = fS / length;
-
-  print('Your average was: ${average.toStringAsFixed(1)}');
-
-  if (fS == length * length) {
-    print('Your score is 100%!');
-  } else if (fS >= fS * .9 && fS < length * length) {
-    print('You placed in the top 90%');
-  } else if (fS >= fS * .8 && fS < fS * .9) {
-    print('You placed in the top 80%');
-  } else if (fS >= fS * .7 && fS < fS * .8) {
-    print('You placed in the top 70%');
-  } else if (fS >= fS * .6 && fS < fS * .7) {
-    print('You placed in the top 60%');
-  } else if (fS >= fS * .5 && fS < fS * .6) {
-    print('You placed in the top 50%');
-  } else if (fS >= fS * .4 && fS < fS * .5) {
-    print('You placed in the top 40%');
-  } else if (fS >= fS * .3 && fS < fS * .4) {
-    print('You placed in the top 30%');
-  } else if (fS >= fS * .2 && fS < fS * .3) {
-    print('You placed in the top 20%');
-  } else if (fS >= fS * .1 && fS < fS * .2) {
-    print('You placed in the top 10%');
-  } else {
-    print('It\'s literally like all you did was choose a difficulty.');
-  }
+  print('Your average mark: ${(fS / length).toStringAsFixed(1)}');
+  print('');
+  print('You scored ${((fS * 100) / (length * 3)).truncate()}%');
 }
